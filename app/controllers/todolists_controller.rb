927b32a024c1724640409838453612f6090b5547
EC2 Default User <ec2-user@ip-172-31-66-105.ec2.internal>
2021-11-02 15:38:19 +0000

    2-4error

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -2,20 +2,25 @@ class TodolistsController < ApplicationController
   def new
     @list=List.new
   end
-  
+
   def create
-    list = List.new(list_params)
+    list=List.new(list_params)
     # １. データを新規登録するためのインスタンス作成
     list.save
     # ２. データをデータベースに保存するためのsaveメソッド実行
-    redirect_to'/top'
+    redirect_to '/top'
      # ３. トップ画面へリダイレクト
   end
-  
+
+  def index
+    @lists=List.all
+  end
+
+
   private
-  
+
   def list_params
-    params.require(:list).permit(:title,:body)
+    params.require(:list).permit(:title, :body)
   end
-    
-  end
+
+end
